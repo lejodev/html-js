cfc5885b4bb59ea3eebbdd4f5888d943535890da
Alejandro Rincón Vera <alejandro.rv97@gmail.com>
2020-03-13 16:01:51 -0500

    Connecting html to js to  retrieve data

---
+++ index.js
@@ -0,0 +1,11 @@
+function displayInfo(){
+    const setText = "";
+    const message = document.getElementById('idName').value;
+    if(!message){
+        console.log("Empty");
+    }
+    else{
+        console.log("Fieofs4g5");
+    }
+    document.getElementById("display").innerHTML = message;
+    }
